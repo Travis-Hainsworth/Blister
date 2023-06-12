105a51d0262934b535c42289378c01ce4fd050ef
Ethan Way <ethan.way@comcast.net>
2023-06-12 08:36:50 -0600

    Working for old data

--- Wheel_Deflection/Python/calculateRadialCompression.py
+++ Wheel_Deflection/Python/calculateRadialCompression.py
@@ -1,9 +1,6 @@
 import numpy as np
 import pandas as pd
-import random
 from scipy.signal import convolve
-from sklearn.neighbors import LocalOutlierFactor
-
 """
 Get the indices of the columns that pertain to the top of the rim marker and the axel marker.
 Could be added upon to include other points of interest.
@@ -19,9 +16,10 @@ rim_top_marker = All the data related to the rim_top marker.
 """
 
 
-def get_useful_markers(mocap_synced, axle_column, rim_top_column):
-    rim_top_marker = mocap_synced.iloc[:, rim_top_column - 1:rim_top_column + 2]
-    axle_marker = mocap_synced.iloc[:, axle_column - 1:axle_column + 2]
+def get_useful_markers(mocap_synced,  axle_column, rim_top_column):
+
+    rim_top_marker = mocap_synced.iloc[:, rim_top_column-1:rim_top_column+2]
+    axle_marker = mocap_synced.iloc[:, axle_column-1:axle_column+2]
 
     return axle_marker, rim_top_marker
 
@@ -42,6 +40,7 @@ radial_compression_smooth = Reduce noise of radial_compression using a moving av
 
 
 def calc_radial_compression(axle_marker_data, rim_top_marker_data, smooth=True):
+
     # For some reason some data points in these data frames are considered type 'str'.
     # These 2 lines make all points into numeric values. It does not remove any data points.
     rim_top_marker_data = rim_top_marker_data.apply(pd.to_numeric, errors='coerce')
@@ -52,12 +51,10 @@ def calc_radial_compression(axle_marker_data, rim_top_marker_data, smooth=True):
     radial_vector = axle_marker_data - rim_top_marker_data
 
     # Calculate Euclidian length for each radial vector.
-    radial_length = np.sqrt(np.sum(radial_vector ** 2, axis=1))
+    radial_length = np.sqrt(np.sum(radial_vector**2, axis=1))
 
     # Calculate radial compression by removing the first value from every value so the data starts at (0, 0)
     radial_compression = [x - radial_length[0] for x in radial_length]
-    [radial_compression.remove(x) for x in radial_compression if x < -.04 or x > .05]
-
     # Calculate the moving average to help reduce noise
     window_size = 50
     radial_compression_smooth = convolve(radial_compression, np.ones(window_size) / window_size, mode='valid')
@@ -66,80 +63,4 @@ def calc_radial_compression(axle_marker_data, rim_top_marker_data, smooth=True):
         return radial_compression
     else:
         return radial_compression_smooth
-
-
-# def replace_outliers_with_mean(data, threshold=3):
-#     median = np.median(data)
-#     mad = np.median([np.abs(x - median) for x in data])
-#     modified_z_scores = [0.6745 * (x - median) / mad for x in data]  # Scaling factor for asymptotic consistency
-#
-#     outliers = np.where(np.abs(modified_z_scores) > threshold)[0]
-#
-#     for index in outliers:
-#         # Find the indices of nearby values within a certain range (e.g., within 2 indices)
-#         nearby_indices = [i for i in range(len(data)) if abs(i - index) <= 1000]
-#
-#         # Calculate the mean of nearby values excluding the outlier
-#         mean = np.mean([data[i] for i in nearby_indices])
-#
-#         # Replace the outlier with the calculated mean
-#         data[index] = mean
-#
-#     return data
-
-# def replace_outliers_with_mean(data, neighbors=500):
-#     # Create an instance of LocalOutlierFactor
-#     lof = LocalOutlierFactor(n_neighbors=neighbors)
-#
-#     # Convert the data list to a NumPy array and reshape it for input to LOF
-#     data_array = np.array(data).reshape(-1, 1)
-#
-#     # Fit the data and obtain the anomaly scores
-#     scores = lof.fit_predict(data_array)
-#
-#     # Replace outliers with the mean of nearby data points
-#     for i, score in enumerate(scores):
-#         if score == -1:
-#             # Reshape the data point to have a single feature
-#             data_point = np.array(data[i]).reshape(1, -1)
-#
-#             # Find the indices of the k nearest neighbors
-#             indices = lof.kneighbors(data_point, n_neighbors=neighbors, return_distance=False)[0]
-#
-#             # Calculate the mean of nearby data points excluding the outlier itself
-#             mean = np.mean([data[idx] for idx in indices if idx != i])
-#
-#             # Replace the outlier with the calculated mean
-#             data[i] = mean
-#
-#     return data
-
-def replace_outliers_with_random(data, neighbors=15, lower_bound=-0.04, upper_bound=0.005):
-    # Create an instance of LocalOutlierFactor
-    lof = LocalOutlierFactor(n_neighbors=neighbors)
-
-    # Convert the data list to a NumPy array and reshape it for input to LOF
-    data_array = np.array(data).reshape(-1, 1)
-
-    # Fit the data and obtain the anomaly scores
-    scores = lof.fit_predict(data_array)
-
-    # Replace outliers with random values based on nearby data points
-    for i, score in enumerate(scores):
-        if score == -1:
-            # Find the indices of the k nearest neighbors within the bounds
-            indices = [idx for idx in lof.kneighbors([[data[i]]], n_neighbors=neighbors, return_distance=False)[0]
-                       if lower_bound <= data[idx] <= upper_bound]
-
-            # Generate a random value based on nearby data points within the bounds
-            random_value = random.choice([data[idx] for idx in indices])
-
-            # Replace the outlier with the random value
-            data[i] = random_value
-
-    return data
-
-
-
-
 
--- Wheel_Deflection/Python/main.py
+++ Wheel_Deflection/Python/main.py
@@ -1,17 +1,21 @@
+import numpy as np
+from matplotlib import ticker
+
 from readInDataFiles import *
 from calculateRadialCompression import *
 import matplotlib.pyplot as plt
 
 # Import mocap data. Make sure every file you want to look at is in the folder you input.
 # Don't remove the "r" before the file path.
-list_mocap_data = getMocapData(r"C:\Users\ethan\Test\ENVE_Static_Mocap_Data_New")
+list_mocap_data = getMocapData(r"C:\Users\ethan\Test\WAOU_Mocap_Data")
 clean_mocap = clean_mocap_data(list_mocap_data)
 
 # Import mts data. Make sure every file you want to look at is in the folder you input.
 # Don't remove the "r" before teh file path.
-list_mts_data = getMTSData(r"C:\Users\ethan\Test\ENVE_Static_MTS_Data_New")
+list_mts_data = getMTSData(r"C:\Users\ethan\Test\WAOU_MTS_Data")
 clean_mts = clean_MTS_data(list_mts_data)
 
+# Make both of the files the same size so they can be graphed.
 synced_data = mocap_synced(clean_mocap, clean_mts)
 
 for counter in range(len(list_mocap_data)):
@@ -19,24 +23,15 @@ for counter in range(len(list_mocap_data)):
     axle_marker_data, rim_top_marker_data = get_useful_markers(synced_data[counter],
                                                                column_names.get_loc('center_hub_y'),
                                                                column_names.get_loc('rim_top_y'))
-
     radial_compression_data = calc_radial_compression(axle_marker_data, rim_top_marker_data)
     radial_compression_data_inches = [i * 0.0393701 for i in radial_compression_data]
-    synced_load = radial_compression_sync(radial_compression_data_inches,
-                                                             clean_mts[counter]['Load (lbf)'].tolist())
-    print(type(synced_load))
-    print(type(radial_compression_data_inches))
-    load_compression_data = pd.DataFrame({'Load (lbf)': synced_load,
-                                          'Compression (in)': radial_compression_data_inches})
 
-    test = replace_outliers_with_random(radial_compression_data_inches)
-    print(type(test))
-    fig, ax = plt.subplots(figsize=(6.4, 4.8))
-    # ax.plot(load_compression_data['Load (lbf)'], load_compression_data['Compression (in)'], linewidth=.25)
-    ax.plot(synced_load, test, linewidth=.25)
+    load_compression_data = pd.DataFrame({'Load (lbf)': np.double(clean_mts[counter]['Load (lbf)']),
+                                          'Compression (in)': np.double(radial_compression_data_inches)})
 
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.plot(load_compression_data['Load (lbf)'], load_compression_data['Compression (in)'])
     plt.ylabel('Compression (in)')
     plt.xlabel('Load (lbf)')
     ax.set_xticks(np.arange(0, 701, 100))
     plt.show()
-
