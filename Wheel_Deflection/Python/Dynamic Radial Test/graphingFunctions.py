8af2d7e013fc2a7200ac2cdd91a301a092e1e9c0
Ethan Way <ethan.way@comcast.net>
2023-07-06 15:00:09 -0600

    made comparison graphing use the mean values of the drop heights

--- Wheel_Deflection/Python/Dynamic Radial Test/graphingFunctions.py
+++ Wheel_Deflection/Python/Dynamic Radial Test/graphingFunctions.py
@@ -81,69 +81,10 @@ def radial_and_lateral_drop_height_plot(file_path, do_you_want_lateral):
     fig.show()
 
 
-def comparison_plot(file_paths, do_you_want_lateral):
-    counter = 0
-    fig = go.Figure()
-    colors = ['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Black', 'Gray', 'Pink', 'Orange']
-
-    for file_path in file_paths:
-        mocap, max_defs, _, height, rim, head = dataProcessingMain(file_path, axis='y')
-
-        max_def_inches = [i * .0393701 for i in max_defs]
-
-        height = np.array(height).astype(float)
-        max_def_inches = np.negative(max_def_inches)
-
-        # Sort the data based on height values
-        sorted_indices = np.argsort(height)
-        sorted_height = height[sorted_indices]
-        sorted_max_def_inches = max_def_inches[sorted_indices]
-
-        # Perform degree 2 polynomial regression
-        regression_coefficients = np.polyfit(sorted_height, sorted_max_def_inches, 2)
-        regression_curve = np.polyval(regression_coefficients, sorted_height)
-
-        r2 = calc_r2(sorted_max_def_inches, regression_curve)
-
-        fig.add_trace(go.Scatter(x=sorted_height, y=regression_curve, mode='lines', line=dict(color=colors[counter]),
-                                 name=f'Regression Curve Radial {rim} {head} head<br>r2 = {r2: .4f}'))
-
-        if do_you_want_lateral == 'yes':
-            mocap_lat, max_defs_lat, _, height_lat, _, _ = dataProcessingMain(file_path, axis='z')
-
-            max_def_inches_lat = [i * .0393701 for i in max_defs_lat]
-
-            height_lat = np.array(height_lat).astype(float)
-            max_def_inches_lat = np.negative(max_def_inches_lat)
-
-            sorted_indices_lat = np.argsort(height_lat)
-            sorted_height_lat = height_lat[sorted_indices_lat]
-            sorted_max_def_inches_lat = max_def_inches_lat[sorted_indices_lat]
-
-            regression_coefficients_lat = np.polyfit(sorted_height_lat, sorted_max_def_inches_lat, 2)
-            regression_curve_lat = np.polyval(regression_coefficients_lat, sorted_height_lat)
-
-            r2_lat = calc_r2(sorted_max_def_inches_lat, regression_curve_lat)
-
-            fig.add_trace(go.Scatter(x=sorted_height_lat, y=regression_curve_lat,
-                                     mode='lines', line=dict(dash='dash', color=colors[counter]),
-                                     name=f'Regression Curve Lateral {rim} {head}<br>r2 = {r2_lat: .4f}'))
-
-        fig.update_layout(
-            title='Max Deformation per Trial',
-            xaxis=dict(title='Drop Height'),
-            yaxis=dict(title='Compression (in)'),
-            showlegend=True,
-            font=dict(size=15)
-        )
-        counter += 1
-    fig.show()
-
-
 def comparison_plot_mean(file_paths):
     counter = 0
     fig = go.Figure()
-    colors = ['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Black', 'Gray', 'Pink', 'Orange']
+    colors = ['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Black', 'Orange', 'Pink', 'Bright Pink']
 
     for file_path in file_paths:
         mocap, max_defs, _, height, rim, head = dataProcessingMain(file_path, axis='y')
@@ -159,23 +100,27 @@ def comparison_plot_mean(file_paths):
         sorted_max_def_inches = max_def_inches[sorted_indices]
 
         mean_values = [np.mean(sorted_max_def_inches[sorted_height == value]) for value in np.unique(sorted_height)]
-        std_values = [np.std(sorted_max_def_inches[sorted_height == value]) for value in np.unique(sorted_height)]
 
-        regression_coefficients = np.polyfit(np.unique(sorted_height), mean_values, 2)
+        regression_coefficients = np.polyfit(np.unique(sorted_height), mean_values, 3)
         regression_curve = np.polyval(regression_coefficients, np.unique(sorted_height))
 
         r2 = calc_r2(mean_values, regression_curve)
 
-        fig.add_trace(go.Scatter(x=np.unique(sorted_height), y=regression_curve,
-                                 mode='lines', line=dict(color=colors[counter]),
-                                 name=f'Regression Curve Lateral {rim} {head}<br>r2 = {r2: .4f}'))
+        if head == 'Flat':
+            fig.add_trace(go.Scatter(x=np.unique(sorted_height), y=regression_curve,
+                                     mode='lines', line=dict(dash='dash', color=colors[counter - 1]),
+                                     name=f'Regression Curve Lateral {rim} {head}<br>r2 = {r2: .4f}'))
+        elif head == 'Rock':
+            fig.add_trace(go.Scatter(x=np.unique(sorted_height), y=regression_curve,
+                                     mode='lines', line=dict(color=colors[counter]),
+                                     name=f'Regression Curve Lateral {rim} {head}<br>r2 = {r2: .4f}'))
 
         fig.update_layout(
             title='Max Deformation per Trial',
             xaxis=dict(title='Drop Height'),
             yaxis=dict(title='Compression (in)'),
             showlegend=True,
-            font=dict(size=15)
+            font=dict(size=15),
         )
         counter += 1
     fig.show()
@@ -226,4 +171,3 @@ def lateral_plot_height(file_path, fig):
                              name=f'Test {head}'))
     fig.add_trace(go.Scatter(x=sorted_height_lat, y=regression_curve_lat, mode='lines',
                              name=f'Degree 2 Regression Curve Lateral {head}<br>r2 = {r2_lat: .4f}'))
-
