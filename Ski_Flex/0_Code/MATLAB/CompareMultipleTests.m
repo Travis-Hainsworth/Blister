99e0f0c26cc57dae707c4e1e4d5b893696f45097
reversedpuppy <jaom6502@colorado.edu>
2023-08-03 11:03:50 -0600

    added 1-10 scaling code to outlier analysis in compare multiple tests

--- Ski_Flex/0_Code/MATLAB/CompareMultipleTests.m
+++ Ski_Flex/0_Code/MATLAB/CompareMultipleTests.m
@@ -41,6 +41,35 @@ hold on
 plot(testAvg)
 title('Average EI data');
 hold off
+%% put to 1-10 scale
+
+binMat = zeros(1,5);
+binMatScale = zeros(1,5);
+%EI = importdata('EI_points.csv').data(:,2);
+EI = testAvg;
+n = (length(EI)/5);
+BlisterSegments = zeros(1,5);
+for i = 1:5
+    start = round(1+(i-1)*n);
+    stop = round(i*n);
+    segment = EI(start:stop);
+    binMat(i) = mean(segment);
+    binMatScale(i) = scaleMe(binMat(i));
+    BlisterSegments(i) = binMatScale(i);
+end
+Tip = BlisterSegments(1);
+Shovel = BlisterSegments(2);
+Underfoot = BlisterSegments(3);
+Heel = BlisterSegments(4);
+Tail = BlisterSegments(5);
+BlisterRank = table(Tip,Shovel,Underfoot,Heel,Tail);
+disp(BlisterRank)
+function value = scaleMe(EI)
+    value = floor(EI/44.575)*.5+4;
+end
+
+
+
 %%
 
 function existing_dirs = check_subdirectories(parent_dir, numbers_list)
